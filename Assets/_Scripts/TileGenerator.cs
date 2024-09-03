2086935d291c40e15d5b050d1a71f95f688aea2a
ant1po1e <symamaxim@gmail.com>
2024-09-03 10:28:33 +0700

    big change to tilegenerator to generate maze, level rearrangements

--- Assets/_Scripts/TileGenerator.cs
+++ Assets/_Scripts/TileGenerator.cs
@@ -5,29 +5,86 @@ using UnityEngine.Tilemaps;
 
 public class TileGenerator : MonoBehaviour
 {
-    public Tilemap tilemap;  
-    public Tile wallTile;    
-    public Vector2Int mapSize = new Vector2Int(10, 10); 
-    public float wallDensity = 0.3f; 
-
-    public void ClearMap()
-    {
-        tilemap.ClearAllTiles();
-    }
+    public Tilemap tilemap;
+    public TileBase wallTile;
+    public TileBase pathTile;  // optional tile for path
+    public Vector2Int mapSize;
+    
+    private bool[,] maze;
 
     public void GenerateMap()
     {
-        ClearMap(); 
+        tilemap.ClearAllTiles();
+
+        maze = new bool[mapSize.x, mapSize.y];
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                maze[x, y] = true;
+            }
+        }
+
+        Vector2Int startPos = new Vector2Int(1, 1);
+        GenerateMaze(startPos);
 
         for (int x = 0; x < mapSize.x; x++)
         {
             for (int y = 0; y < mapSize.y; y++)
             {
-                if (Random.value < wallDensity)
+                Vector3Int tilePosition = new Vector3Int(x, y, 0);
+                if (maze[x, y])
                 {
-                    tilemap.SetTile(new Vector3Int(x, y, 0), wallTile);
+                    tilemap.SetTile(tilePosition, wallTile);
+                }
+                else if (pathTile != null)
+                {
+                    tilemap.SetTile(tilePosition, pathTile); 
                 }
             }
+        }
+    }
+
+    private void GenerateMaze(Vector2Int pos)
+    {
+        maze[pos.x, pos.y] = false;
+
+        // Define the directions (up, down, left, right) in random order
+        List<Vector2Int> directions = new List<Vector2Int>
+        {
+            new Vector2Int(0, 2),  // Up
+            new Vector2Int(0, -2), // Down
+            new Vector2Int(2, 0),  // Right
+            new Vector2Int(-2, 0)  // Left
+        };
+        Shuffle(directions);
+
+        foreach (Vector2Int dir in directions)
+        {
+            Vector2Int newPos = pos + dir;
+            Vector2Int betweenPos = pos + dir / 2;
+
+            if (IsInBounds(newPos) && maze[newPos.x, newPos.y])
+            {
+                maze[betweenPos.x, betweenPos.y] = false;
+                GenerateMaze(newPos);
+            }
+        }
+    }
+
+    private bool IsInBounds(Vector2Int pos)
+    {
+        return pos.x > 0 && pos.y > 0 && pos.x < mapSize.x - 1 && pos.y < mapSize.y - 1;
+    }
+
+    private void Shuffle<T>(List<T> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            T temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
         }
     }
 }
